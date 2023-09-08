9947c957bd6a448abb1c04f8a1438e69dd1f71d5
Phonrathana.github.io <127619322+phonRathana@users.noreply.github.com>
2023-09-08 07:02:23 +0700

    index.html

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createWindow () {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-  win.loadFile('index.html')
+  window.loadFile('index.html')
 }
 app.whenReady().then(() => {
   createWindow()
