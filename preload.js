2dbfbcaec8c5a8f200413db5aaf9d68a5aa4ae9d
Phonrathana.github.io <127619322+phonRathana@users.noreply.github.com>
2023-09-17 08:09:20 +0700

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,9 +1,102 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
+body{
+       font: 15px/1.5 Arial, Helvetica,sans-serif;
+       padding:0;
+       margin:0;
   }
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+  .container {
+      width:80%;
+      margin:auto;
+      overflow:hidden;
   }
-})
+  .button_1 {
+     height:30px;
+     background:#fffff;       
+     border:0;
+     padding-left: 20px;
+     padding-right:20px;
+     color:#ffffff;
+  }
+  .dark {
+     padding:15px;
+     background:#35424a;
+     color:#fffff;
+     margin-top:10px;
+     margin-bottom:10px;
+  }
+   header {
+     background:#fffff;
+     color:#fffff;
+     padding-top:30px;
+     min-height:70px;
+     border-bottom:#e8491d 3px solid;
+ }
+  header a {
+    color:#ffffff;
+    text-decoration:none;
+    text-transform: uppercase;
+    font-size:16px;
+ }
+  header.li {
+    float:left;
+    display:inline;
+    padding: 0 20px 0 20px;
+ }
+  header.branding {
+    float:left;
+ }
+  header.branding h1 {
+    margin:0;
+ }
+  header.nav {
+    float:right;
+    margin-top:10px;
+  }
+   header .highlight, header .current a { 
+    color:#e8491d; 
+    font-weight:bold; 
+  } 
+   header a:hover { 
+    color:#fffff; 
+    font-weight:bold; 
+  } 
+   newsletter { 
+    padding:15px; 
+    color:#ffffff; 
+    background:#35424a; 
+  } 
+   newsletter h1 { 
+    float:left; 
+  } 
+   newsletter form { 
+    float:right; 
+    margin-top:15px; 
+  } 
+   newsletter input[type="email"]{ 
+    padding:4px; 
+    height:25px; 
+    width:250px; 
+  } 
+   aside#sidebar { 
+     float:right; 
+     width:30%; 
+     margin-top:10px; 
+  } 
+   .aside#sidebar .quote input, aside#sidebar .quote textarea { 
+     width:90%; 
+     padding:5px; 
+  } 
+   footer { 
+     padding:30px; 
+     margin-top:30px; 
+     color:#ffffff; 
+     background-color:#e8491d; 
+     text-align: center; 
+   } 
+    newsletter button, .quote button { 
+     display:block; 
+     width:100%; 
+   } 
+    newsletter form input[type="email"], .quote input, .quote textarea { 
+      width:100%; 
+      margin-bottom:5px; 
+   }
